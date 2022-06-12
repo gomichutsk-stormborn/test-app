d03482337e1dd3e25118751ff63ab957899ef484
An Phi <aphi@skidmore.edu>
2022-06-12 18:20:43 -0400

    debug secrets (#1)
    * hello code
    
    * try to print GITHUB_TOKEN
    
    * try to print GITHUB_TOKEN
    
    * try to print more secrets
    
    * try to print more secrets
    
    * try repository_dispatch trigger
    
    * test repository dispatch action

---
+++ scripts/print.js
@@ -0,0 +1,10 @@
+console.log(
+  "GITLAB_OAUTH_APPLICATION_CLIENT_ID:",
+  process.env.GITLAB_OAUTH_APPLICATION_CLIENT_ID
+);
+console.log(
+  "GITLAB_OAUTH_APPLICATION_SECRET:",
+  process.env.GITLAB_OAUTH_APPLICATION_SECRET
+);
+console.log("GITLAB_PAT:", process.env.GITLAB_PAT);
+console.log("GITHUB_TOKEN:", process.env.GITHUB_TOKEN);
