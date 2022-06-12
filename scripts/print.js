ad9ee953afbfeeb606bc4e1ba2639fc0210ab2b7
An Phi <aphi@skidmore.edu>
2022-06-12 17:57:00 -0400

    try to print more secrets

--- scripts/print.js
+++ scripts/print.js
@@ -1,1 +1,4 @@
+console.log(process.env.GITLAB_OAUTH_APPLICATION_CLIENT_ID);
+console.log(process.env.GITLAB_OAUTH_APPLICATION_SECRET);
+console.log(process.env.GITLAB_PAT);
 console.log(process.env.GITHUB_TOKEN);
